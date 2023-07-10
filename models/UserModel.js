136863436f7be8074400515f8f97d52a34aa515d
Krishna Gupta <121439479+krishnagptamcs@users.noreply.github.com>
2023-07-10 13:33:40 +0530

    connecting to db

---
+++ models/UserModel.js
@@ -0,0 +1,33 @@
+const mongoose = require("mongoose");
+
+const UserSchema = new mongoose.Schema({
+
+    name:{
+        type: String,
+        require:true,
+    },
+    userId:{
+        type:String,
+        require:true,
+        unique : true
+    },
+    password:{
+        type:String,
+        require:true
+    },
+    email:{
+        type:String,
+        require:true,
+        unique:true,
+        lowercase:true //convert all emails to lower case for easy searching and comparison in database
+
+    },
+    userType:{
+        type:String,
+        default:'CUSTOMER',
+        enum:['ADMIN','CUSTOMER'], // dollection of possible value of a field 
+        require:true
+    }
+},{timestamps:true})
+
+module.exports = mongoose.model("User", UserSchema);
--- server.js
+++ server.js
@@ -1,8 +1,26 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const dbConfig = require("./configs/db.config");
+
+
+
 
 const app = express();
+mongoose.connect(dbConfig.DB_URL);
 
-const serverConfig = require("./configs/server.config")
+const db = mongoose.connection;
+
+db.on("error" ,()=>{
+    console.log("error while connecting database")
+} )
+
+db.once("open" ,()=>{
+    console.log("db is connected succesfullly")
+} );
+
+
+const serverConfig = require("./configs/server.config");
+
 
 app.listen(serverConfig.PORT, ()=>{
     console.log(`Server is running on port ${serverConfig.PORT}`)
